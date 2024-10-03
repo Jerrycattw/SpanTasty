3984454a0d8bb60ef868a91a158b646438f5ba6b
Kelly <kelly005499@yahoo.com.tw>
2024-10-04 00:54:37 +0800

    20241004 12:55 ShoppingOrder CRUD

--- src/main/java/com/eatspan/SpanTasty/repository/store/ProductRepository.java
+++ src/main/java/com/eatspan/SpanTasty/repository/store/ProductRepository.java
@@ -1,13 +1,19 @@
 package com.eatspan.SpanTasty.repository.store;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eatspan.SpanTasty.entity.store.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	@Query("SELECT p FROM Product p JOIN FETCH p.productType")
     List<Product> findAllProduct();
+	
+	
+	@Query("SELECT p.productPrice FROM Product p WHERE p.id = :productId")
+	Integer findProductPriceByProductId(@Param("productId") Integer productId);
 }
